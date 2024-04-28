ad8338e7c1fd767764282f22266d67ab5dd839ef
kareem7199 <kareemtameregy@gmail.com>
2024-04-28 23:40:57 +0300

    Register Endpoint

--- Talabat.APIs/Controllers/AccountController.cs
+++ Talabat.APIs/Controllers/AccountController.cs
@@ -12,8 +12,8 @@ namespace Talabat.APIs.Controllers
 		private readonly UserManager<ApplicationUser> _userManager;
 		private readonly SignInManager<ApplicationUser> _signInManager;
 
-		public AccountController(UserManager<ApplicationUser> userManager , SignInManager<ApplicationUser> signInManager)
-        {
+		public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+		{
 			_userManager = userManager;
 			_signInManager = signInManager;
 		}
@@ -22,23 +22,47 @@ namespace Talabat.APIs.Controllers
 		public async Task<ActionResult<UserDto>> Login(LoginDto model)
 		{
 			var user = await _userManager.FindByEmailAsync(model.Email);
-			
-			if (user == null)
-				return Unauthorized(new ApiResponse(401 , "invalid login"));
 
-			var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password , false);
+			if (user == null)
+				return Unauthorized(new ApiResponse(401, "invalid login"));
+
+			var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
 
 			if (!result.Succeeded)
 				return Unauthorized(new ApiResponse(401, "invalid login"));
 
 			return Ok(new UserDto()
 			{
-				DisplayName = user.DisplayName ,
-				Email = user.Email ,
+				DisplayName = user.DisplayName,
+				Email = user.Email,
 				Token = "this will be token"
 			});
 
 		}
 
-    }
+		[HttpPost("register")]
+		public async Task<ActionResult<UserDto>> Register(RegisterDto model)
+		{
+			var user = new ApplicationUser()
+			{
+				DisplayName = model.DisplayName,
+				Email = model.Email,
+				UserName = model.Email.Split('@')[0],
+				PhoneNumber = model.Phone
+			};
+
+			var result = await _userManager.CreateAsync(user , model.Password);
+
+			if (!result.Succeeded)
+				return BadRequest(new ApiValidationErrorResponse() { Errors = result.Errors.Select((E) => E.Description)});
+
+			return Ok(new UserDto()
+			{
+				DisplayName = user.DisplayName ,
+				Email = user.Email,
+				Token = "Token"
+			});
+		}
+
+	}
 }
