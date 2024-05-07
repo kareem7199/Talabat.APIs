351f73bc45a83cd2ac11d6660b91b18f4ca99abb
kareem7199 <kareemtameregy@gmail.com>
2024-05-07 16:53:44 +0300

    OrderToReturnDto

--- Talabat.APIs/Controllers/OrdersController.cs
+++ Talabat.APIs/Controllers/OrdersController.cs
@@ -23,10 +23,10 @@ namespace Talabat.APIs.Controllers
 			_mapper = mapper;
 		}
 
-		[ProducesResponseType(typeof(Order) , StatusCodes.Status200OK)]
+		[ProducesResponseType(typeof(OrderToReturnDto) , StatusCodes.Status200OK)]
 		[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
 		[HttpPost]
-		public async Task<ActionResult<Order>> CreateOrder(OrderDto orderDto)
+		public async Task<ActionResult<OrderToReturnDto>> CreateOrder(OrderDto orderDto)
 		{
 			var address = _mapper.Map<Address>(orderDto.ShippingAddress);
 			
@@ -36,24 +36,24 @@ namespace Talabat.APIs.Controllers
 
 			if (order is null) return BadRequest(new ApiResponse(404));
 
-			return Ok(order);
+			return Ok(_mapper.Map<OrderToReturnDto>(order));
 		}
 
 		[HttpGet]
-		public async Task<ActionResult<IReadOnlyList<Order>>> GetOrdersForUser()
+		public async Task<ActionResult<IReadOnlyList<OrderToReturnDto>>> GetOrdersForUser()
 		{
 
 			var email = User.FindFirstValue(ClaimTypes.Email);
 
 			var orders = await _orderService.GetOrdersForUserAsync(email);
 
-			return Ok(orders);
+			return Ok(_mapper.Map<IReadOnlyList<OrderToReturnDto>>(orders));
 		}
 
-		[ProducesResponseType(typeof(Order), StatusCodes.Status200OK)]
+		[ProducesResponseType(typeof(OrderToReturnDto), StatusCodes.Status200OK)]
 		[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
 		[HttpGet("{id}")]
-		public async Task<ActionResult<Order>> GetOrderForUser(int id)
+		public async Task<ActionResult<OrderToReturnDto>> GetOrderForUser(int id)
 		{
 			var email = User.FindFirstValue(ClaimTypes.Email);
 
@@ -61,7 +61,7 @@ namespace Talabat.APIs.Controllers
 
 			if (order is null) return NotFound(new ApiResponse(404));
 
-			return Ok(order);
+			return Ok(_mapper.Map<OrderToReturnDto>(order));
 		}
 	}
 }
--- Talabat.APIs/Helpers/MappingProfile.cs
+++ Talabat.APIs/Helpers/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Talabat.APIs.Dtos;
 using Talabat.Core.Entities;
+using Talabat.Core.Entities.Order_Aggregate;
 using Talabat.Core.Identity;
 
 namespace Talabat.APIs.Helpers
@@ -16,8 +17,18 @@ namespace Talabat.APIs.Helpers
 
 			CreateMap<CustomerBasketDto, CustomerBasket>();
 			CreateMap<BasketItemDto, BasketItem>();
-			CreateMap<Address, AddressDto>().ReverseMap();
+			CreateMap<Core.Identity.Address, AddressDto>().ReverseMap();
 			CreateMap<AddressDto, Core.Entities.Order_Aggregate.Address>();
+
+			CreateMap<Order, OrderToReturnDto>()
+				.ForMember(D => D.DeliveryMethod , O => O.MapFrom(S => S.DeliveryMethod.ShortName))
+				.ForMember(D => D.DeliveryMethodCost , O => O.MapFrom(S => S.DeliveryMethod.Cost));
+
+			CreateMap<OrderItem, OrderItemDto>()
+				.ForMember(D => D.ProductId, O => O.MapFrom(S => S.Product.ProductId))
+				.ForMember(D => D.ProductName, O => O.MapFrom(S => S.Product.ProductName))
+				.ForMember(D => D.PictureUrl , O => O.MapFrom(S => S.Product.PictureUrl))
+				.ForMember(D => D.PictureUrl , O => O.MapFrom<OrderItemPictureUrlResolver>());
 
 		}
 	}
